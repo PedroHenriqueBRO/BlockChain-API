2dd0900a31d3c592c8c6b8bd3eb6b5816068eb6e
Pedro <xtron44@gmail.com>
2025-04-15 22:24:39 -0300

    Em block controller agora só é preciso passar Data e assim gerar um bloco pela lógica de NewBlock() onde os valores são setados verificando suas integridades

--- controller/block-controller.go
+++ controller/block-controller.go
@@ -3,11 +3,15 @@ package controller
 import (
 	"block-chain/model"
 	"block-chain/usecase"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Stringaux struct {
+	Aux string `json:"Data"`
+}
 type BlockController struct {
 	Blockusecase usecase.Blockusecase
 }
@@ -27,11 +31,19 @@ func (b *BlockController) GetBlocks(ctx *gin.Context) {
 }
 func (b *BlockController) InsertBlock(ctx *gin.Context) {
 	var block model.Block
-	err := ctx.BindJSON(&block)
+	aux := Stringaux{}
+	err := ctx.BindJSON(&aux)
+	if err != nil {
+		fmt.Println("Nao foi possível passar de JSON para Block")
+	}
+	blocos, err := b.Blockusecase.GetBlocks()
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, err)
 		return
 	}
+	block.Data = []byte(aux.Aux)
+	block.Previoushash = blocos[len(blocos)-1].Hash
+	block.NewBlock()
 	insertedblock, err := b.Blockusecase.InsertBlock(block)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)

8c75b4b6006945f033e755ef749117d77c2724bf
Pedro <xtron44@gmail.com>
2025-04-16 22:57:36 -0300

    Adiciono o bloco genesis apenas se a blockchain estiver vazia, isso não havia sido verificado nas versões anteriores do código

--- cmd/main.go
+++ cmd/main.go
@@ -18,20 +18,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	bloco := model.Block{Timestamp: time.Now().Unix(), Data: []byte("Genesis"), Previoushash: []byte("")}
-	pow := model.NewProofOfWork(&bloco)
-	bloco.Nonce, bloco.Hash = pow.Run()
 	blockrepository := repository.Newblockrepository(dbconnection)
 	blockusecase := usecase.Newblockusecase(blockrepository)
 	blockController := controller.NewblockController(blockusecase)
-	_, err = blockrepository.InsertBlock(bloco)
+	blocoaux, err := blockController.Blockusecase.GetLastBlock()
 	if err != nil {
 		fmt.Println(err)
+	}
+	if blocoaux.Timestamp == 0 {
+		bloco := model.Block{Timestamp: time.Now().Unix(), Data: []byte("Genesis"), Previoushash: []byte("")}
+		pow := model.NewProofOfWork(&bloco)
+		bloco.Nonce, bloco.Hash = pow.Run()
+		_, err = blockrepository.InsertBlock(bloco)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	server.GET("/blocks", blockController.GetBlocks)
 	server.POST("/block", blockController.InsertBlock)
 	server.DELETE("/delete", blockController.Deleteall)
 	server.GET("/getbyhash", blockController.GetByHash)
+	server.GET("/getlblock", blockController.GetLastBlock)
 	server.Run(":8000")
 
 }
